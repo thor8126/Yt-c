cac62375a5d058e2ecf5e97f699baee79042f3d5
thor8126 <lakhabhayi.com>
2023-12-14 16:13:58 +0530

    removed config

--- src/App.jsx
+++ src/App.jsx
@@ -26,30 +26,22 @@ export default function App() {
           ref={playerRef}
           url={url}
           controls
-          width="100%"
-          height="100%"
-          autoPlay
+          width="80%"
+          height="80%"
+          autoPlay={true}
+          muted={true}
           playing
           pip
           progressInterval={100}
           onProgress={(state) => setPlayed(state.played)}
-          config={{
-            youtube: {
-              playerVars: {
-                // Add any additional YouTube player parameters here
-              },
-            },
-          }}
           onContextMenu={(e) => e.preventDefault()}
-          onUpdate={(state) => console.log("onUpdate", state)}
-          onEnded={() => console.log("onEnded")}
         />
       </div>
       <div>
         <label>
           Select Quality:
           <select value={quality} onChange={(e) => handleQualityChange(e.target.value)}>
-            <option value="2k">2k</option>
+            <option value="2k">4k</option>
             <option value="1080">1080p</option>
             <option value="720">720p</option>
           </select>
